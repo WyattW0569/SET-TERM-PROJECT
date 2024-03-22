6ff761c1a04e5496dd54d81b0d5f2f32e5c79650
Wyatt W <wywal7668@gmail.com>
2024-03-22 14:33:44 -0700

    ADDED MORE BUTTON FUNCTIONALITY

--- web-impl/main.py
+++ web-impl/main.py
@@ -7,8 +7,9 @@ import gc
 
 
 onboard = Pin("LED", Pin.OUT, value=0)
+#TEMP VARS
 test = "start"
-test2 = 0
+minTemp = 0
 
 ssid = 'SET'       #Set access point name 
 password = 'Grunklestan'      #Set your access point password
@@ -27,7 +28,7 @@ s.listen(5)
 
 def get_html(html_name,css_name):
    with open(html_name, 'r') as file:
-      html = file.read().format(Current_temp=test,MIN=test2,MAX="max",outdoor_temp="outdoor",light_mode="light mode")
+      html = file.read().format(Current_temp=test,MIN=minTemp,MAX="max",outdoor_temp="outdoor",light_mode="light mode")
       file.close()
    with open(css_name, 'r') as file1:
       css = file1.read()
@@ -49,9 +50,11 @@ while True:
         request = request.split()[1]
     except IndexError:
         pass
-    if request == '/test?':
+    if request == '/mintempup?':
         test='IT WORKED!'
-        test2+=1
+        minTemp+=1
+    if request == '/mintempdown?':
+        minTemp-=1
     elif request == '/lightoff?':
         onboard.off()
     gc.collect()
